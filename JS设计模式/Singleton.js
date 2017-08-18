088ccf2e70e1578fd709158b68f43d71487ccf04
yangkun <357821351@qq.com>
2017-08-18 14:34:37 +0800

    设计模式学习

---
+++ JS设计模式/Singleton.js
@@ -0,0 +1,10 @@
+// 惰性单例，只在需要的时候才创建实例
+Singleton.getInstance = (function(){
+		var instance = null;
+		return function( name ){
+			if ( !instance ){
+				instance = new Singleton( name );
+			}
+			return instance;
+		}
+	})();
